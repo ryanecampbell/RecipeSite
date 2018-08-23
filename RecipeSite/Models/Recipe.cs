a733bb1dc7302d9904d04c7f0d87e43d79becfbc
Ryan Campbell <campbell.ryan.e@gmail.com>
2018-08-22 21:46:24 -0400

    Add project files.

---
+++ RecipeSite/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace RecipeSite.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class RecipeDBContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
